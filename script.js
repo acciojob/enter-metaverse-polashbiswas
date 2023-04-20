d47534f67c9e4ab3892cd03fbf61bd2fccf484b6
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-20 14:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+
+button.addEventListener("click", (event) => {
+	para.innerHTML = `<h1>Entered Metaverse</h1>`;
+});
